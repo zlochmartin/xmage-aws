3eb3df18a33048360fa57986fedf07a98ed24d4f
Martin Zloch <zlochmartin@gmail.com>
2022-11-11 20:43:46 +0100

    Initial commit

---
+++ test/xmage-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as XmageAws from '../lib/xmage-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/xmage-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new XmageAws.XmageAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
